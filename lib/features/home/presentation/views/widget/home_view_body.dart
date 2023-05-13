c416930d856f3ab4131b456afd6481ee00573a9a
EMADOMARAH <emadomarah2020@gmail.com>
2023-05-14 00:26:39 +0300

    create app styles

--- lib/features/home/presentation/views/widget/home_view_body.dart
+++ lib/features/home/presentation/views/widget/home_view_body.dart
@@ -14,9 +14,40 @@ class HomeViewBody extends StatelessWidget {
         CustomAppBar(),
         FeaturedListViewItem(),
         SizedBox(height: 50,),
-        Text('Best Seller' , style: Styles.titleMedium,)
+        Text('Best Seller' , style: Styles.textStyle18,)
 
       ],
+    );
+  }
+}
+
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+              aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.red,
+                image: const DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage(AssetsHelper.logo)
+                ),
+              ),
+            ),
+
+          ),
+
+        ],
+      ),
     );
   }
 }
