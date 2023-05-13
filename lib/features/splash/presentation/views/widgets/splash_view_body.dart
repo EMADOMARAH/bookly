ce9a209dd38a73109658a4a2dd1b6a21d7729c5f
EMADOMARAH <emadomarah2020@gmail.com>
2023-05-13 21:04:03 +0300

    refactor init state & navigate to home

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,6 +1,10 @@
+import 'package:bookly/core/constants.dart';
 import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../../../home/presentation/views/home_viewe.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({Key? key}) : super(key: key);
@@ -18,6 +22,17 @@ class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProvid
   void initState() {
     // TODO: implement initState
     super.initState();
+    initSlidingAnimation();
+    navigateToHome();
+  }
+
+  void navigateToHome() {
+    Future.delayed(const Duration(seconds: 2) , () {
+      Get.to(()=> const HomeView(), transition: Transition.fade , duration: kTransitionDuration);
+    });
+  }
+
+  void initSlidingAnimation() {
     animationController = AnimationController(vsync: this, duration: const Duration(minutes: 1));
     slidingAnimation = Tween<Offset>(begin: Offset(0, 2), end: Offset.zero).animate(animationController);
     animationController.forward();
