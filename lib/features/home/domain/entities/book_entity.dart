cc5402f5114cd6b0e4531d0c4fa8864073f4c2ba
EMADOMARAH <emadomarah2020@gmail.com>
2023-05-26 15:53:10 +0300

    add book entities , add home repo

---
+++ lib/features/home/domain/entities/book_entity.dart
@@ -0,0 +1,10 @@
+class BookEntity{
+  final String image;
+  final String tital ;
+  final String authorName ;
+  final num price ;
+  final num rating ;
+
+  BookEntity(this.image, this.tital, this.authorName, this.price, this.rating);
+
+}
---
+++ lib/features/home/domain/repo/home_repo.dart
@@ -0,0 +1,9 @@
+import '../entities/book_entity.dart';
+
+abstract class HomeRepo{
+
+  Future<List<BookEntity>> fetchFeaturedBooks();
+
+  Future<List<BookEntity>> fetchNewestBooks();
+
+}
