8dae264a96a28113102263a7d4dfc3c11b26ea66
EMADOMARAH <emadomarah2020@gmail.com>
2023-06-02 16:51:06 +0300

    cache featured books

--- lib/features/home/data/data_source/home_remote_data_source.dart
+++ lib/features/home/data/data_source/home_remote_data_source.dart
@@ -1,5 +1,8 @@
+import 'package:bookly/core/constants.dart';
 import 'package:bookly/core/utils/api_service.dart';
+import 'package:bookly/core/utils/functions/save_books.dart';
 import 'package:bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:hive/hive.dart';
 
 import 'package:bookly/Features/home/domain/entities/book_entity.dart';
 
@@ -22,8 +25,12 @@ class HomeRemoteDataSourceImpl extends HomeRemoteDataSource{
     
     List<BookEntity> booksList = getBooksList(data);
 
+    saveBooksData(booksList, KFeaturedBox);
+
     return booksList;
   }
+
+
 
   List<BookEntity> getBooksList(Map<String, dynamic> data) {
     List<BookEntity> booksList = [] ;
