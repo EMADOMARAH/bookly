bda4d63a7e3dfd81ed056aeb1294139d061490b9
EMADOMARAH <emadomarah2020@gmail.com>
2023-06-04 16:09:56 +0300

    Add DI with getit library create Simple observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:bookly/core/constants.dart';
+import 'package:bookly/core/utils/api_service.dart';
 import 'package:bookly/core/utils/app_router.dart';
+import 'package:bookly/core/utils/functions/setup_service_locator.dart';
+import 'package:bookly/core/utils/simple_bloc_observer.dart';
+import 'package:bookly/features/home/data/data_source/home_local_data_source.dart';
+import 'package:bookly/features/home/data/data_source/home_remote_data_source.dart';
+import 'package:bookly/features/home/data/repo/home_repo_impl.dart';
 import 'package:bookly/features/home/domain/entities/book_entity.dart';
+import 'package:bookly/features/home/domain/use_cases/fetch_featured_books_use_case.dart';
+import 'package:bookly/features/home/domain/use_cases/fetch_newest_books_use_case.dart';
+import 'package:bookly/features/home/presentation/cubit/featued_books_cubit/featured_books_cubit.dart';
+import 'package:bookly/features/home/presentation/cubit/newest_books_cubit/newest_books_cubit.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive/hive.dart';
@@ -9,16 +22,19 @@ import 'package:hive_flutter/adapters.dart';
 
 import 'features/splash/presentation/views/splash_view.dart';
 
-void main() async{
+void main() async {
   await Hive.initFlutter();
   Hive.registerAdapter(BookEntityAdapter());
+
+  setupServiceLocator();
+
   await Hive.openBox<BookEntity>(KFeaturedBox);
   await Hive.openBox<BookEntity>(KNewestBox);
 
+  Bloc.observer = SimpleBlocObserver();
+
 
   runApp(const BooklyApp());
-
-
 }
 
 class BooklyApp extends StatelessWidget {
@@ -26,14 +42,35 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor:kPrimaryColor,
-      textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) {
+            return FeaturedBooksCubit(
+              FetchFeaturedBooksUseCase(
+               getIt.get<HomeRepoImpl>(),
+              ),
+            );
+          },
+        ),
+        BlocProvider(
+          create: (context) {
+            return NewestBooksCubit(
+              FetchNewestBooksUseCase(
+                getIt.get<HomeRepoImpl>(),
+              ),
+            );
+          },
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+        ),
       ),
-
     );
   }
 }
-
