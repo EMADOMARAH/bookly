beb65f87a1b26864a470787f99ceaf4c79aa8065
EMADOMARAH <emadomarah2020@gmail.com>
2023-06-04 15:07:00 +0300

    create server failure from home repo impl

--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -1,3 +1,48 @@
- class Failure{
+import 'package:dio/dio.dart';
 
- }
+abstract class Failure {
+  final String message;
+
+  Failure(this.message);
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(super.message);
+
+  factory ServerFailure.fromDioError(DioError e)
+  {
+    switch(e.type){
+
+      case DioErrorType.connectionTimeout:
+        return ServerFailure("Connection timeout");
+      case DioErrorType.sendTimeout:
+        return ServerFailure("send timeout");
+      case DioErrorType.receiveTimeout:
+        return ServerFailure("receive timeout");
+      case DioErrorType.badCertificate:
+        return ServerFailure("Bad Certificate");
+      case DioErrorType.badResponse:
+        return ServerFailure.fromResponse(e.response!.statusCode, e.response!.data);
+      case DioErrorType.cancel:
+        return ServerFailure("Request or ApiServer was canceled");
+      case DioErrorType.connectionError:
+        return ServerFailure("No Internet Connection");
+      case DioErrorType.unknown:
+        return ServerFailure("Opps there was an Error, PLease try again");
+    }
+  }
+
+  factory ServerFailure.fromResponse(int? statusCode , dynamic response)
+  {
+    if (statusCode == 404) {
+      return ServerFailure("Your request was not found , please try later");
+    }else if (statusCode == 500) {
+      return ServerFailure("THere is a problem with server, please try later");
+    }else if (statusCode == 400 ||statusCode == 401 ||statusCode == 403) {
+      return ServerFailure(response['error']['message']);
+    }else{
+      return ServerFailure("there was an error, please try again");
+    }
+  }
+}
+
--- lib/features/home/data/repo/home_repo_impl.dart
+++ lib/features/home/data/repo/home_repo_impl.dart
@@ -4,47 +4,54 @@ import 'package:bookly/features/home/data/data_source/home_remote_data_source.da
 import 'package:bookly/features/home/domain/entities/book_entity.dart';
 import 'package:bookly/features/home/domain/repo/home_repo.dart';
 import 'package:dartz/dartz.dart';
-import 'package:hive/hive.dart';
+import 'package:dio/dio.dart';
 
-class HomeRepoImpl extends HomeRepo{
+class HomeRepoImpl extends HomeRepo {
+  final HomeRemoteDataSource homeRemoteDataSource;
 
-  final HomeRemoteDataSource homeRemoteDataSource ;
-  final HomeLocalDataSource homeLocalDataSource ;
+  final HomeLocalDataSource homeLocalDataSource;
 
-  HomeRepoImpl({required this.homeRemoteDataSource,required this.homeLocalDataSource});
+  HomeRepoImpl({required this.homeRemoteDataSource, required this.homeLocalDataSource});
 
   @override
-  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks()async {
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() async {
     try {
-      var booksList = homeLocalDataSource.fetchFeaturedBooks();
+      List<BookEntity> booksList;
+      booksList = homeLocalDataSource.fetchFeaturedBooks().cast<BookEntity>();
       if (booksList.isNotEmpty) {
         return right(booksList.cast<BookEntity>());
-
       }
-      var books = await homeRemoteDataSource.fetchFeaturedBooks();
+      booksList = (await homeRemoteDataSource.fetchFeaturedBooks()).cast<BookEntity>();
 
-      return right(books.cast<BookEntity>());
-    }catch (e) {
-      return left(Failure());
+      return right(booksList.cast<BookEntity>());
+    } catch (e) {
+      if (e is DioError) {
+        return left(ServerFailure.fromDioError(e));
+      }else{
+        return left(ServerFailure(e.toString()));
+      }
+
     }
   }
 
   @override
   Future<Either<Failure, List<BookEntity>>> fetchNewestBooks() async {
     try {
-      var booksList = homeLocalDataSource.fetchNewestBooks();
-      if (booksList.isNotEmpty) {
-        return right(booksList.cast<BookEntity>());
-
+      List<BookEntity> books;
+      books = homeLocalDataSource.fetchNewestBooks().cast<BookEntity>();
+      if (books.isNotEmpty) {
+        return right(books.cast<BookEntity>());
       }
-      var books = await homeRemoteDataSource.fetchNewestBooks();
+      books = (await homeRemoteDataSource.fetchNewestBooks()).cast<BookEntity>();
 
       return right(books.cast<BookEntity>());
-    }catch (e) {
-      return left(Failure());
+    } catch (e) {
+      if (e is DioError) {
+        return left(ServerFailure.fromDioError(e));
+      }else{
+        return left(ServerFailure(e.toString()));
+      }
+
     }
   }
-
-
-
 }
